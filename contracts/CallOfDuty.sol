50f08bd0a2be8f297788367269d29cb3fc14d343
droidpulkit <pulkitkumar190@gmail.com>
2021-10-02 18:35:46 -0400

    Initializing the Call of Duty NFT project

---
+++ contracts/CallOfDuty.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+//Extending from openZeppelin contract
+contract CallOfDuty is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    //This constructor takes 2 params
+    //Name of NFT Token, NFT symbol
+    constructor() ERC721("SquareNFT", "SQUARE") {
+        console.log("This is my NFT contract, more things are coming soon!!");
+    }
+
+    function makeCallOfDutyNFT() public {
+        //Get the current tokenId, this start at 0
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/DNO4");
+        console.log(
+            "An NFT w/ ID %s has been minted to %s",
+            newItemId,
+            msg.sender
+        );
+
+        _tokenIds.increment();
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+//Deploy.js is used to deploy to staging env
+
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('CallOfDuty')
+    const nftContract = await nftContractFactory.deploy()
+    await nftContract.deployed()
+    console.log("Contract deployed to:", nftContract.address)
+
+    //Minting 1st NFT
+    let txn = await nftContract.makeCallOfDutyNFT()
+    await txn.wait()
+
+    //Minting 2nd NFT
+    txn = await nftContract.makeCallOfDutyNFT()
+    await txn.wait()
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
